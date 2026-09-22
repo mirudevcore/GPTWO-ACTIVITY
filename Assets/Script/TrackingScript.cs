e41c85fc00225c7cd7ab6015e76773fe47c03fd1
ciit <hadajamiru@gmail.com>
2026-09-22 13:00:02 +0800

    update

---
+++ Assets/Script/TrackingScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TrackingScript : MonoBehaviour
+
+{
+    public float rotationSpeed;
+    public Transform target;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = (target.position - transform.position ).normalized;
+        Quaternion targetRot = Quaternion.LookRotation(dir);
+        transform.rotation =  Quaternion.Slerp (transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
+        float dot = Vector3.Dot(transform.forward, dir);
+
+    }
+}
